53362670b7f9309f13b5156ab84437f57fb00c0c
bruno.lima <brunopinheirolima9@gmail.com>
2024-05-13 18:01:36 -0400

    Novidades

--- Desafio_em_Python/Desafio_2.py
+++ Desafio_em_Python/Desafio_2.py
@@ -1,19 +1,21 @@
-consumo = 0    
-def recomendar_plano():
-    global consumo
+# Desafio 1 da dio.me
+
+def recomendar_plano(consumo):
+  
     if consumo <= 10:
-        print("Plano Essencial")
+        print("Plano Essencial Fibra - 50Mbps")
         
-    if consumo == 10 and consumo <= 20:
-        print("Plano Prata Fibra")
+    if consumo > 10 and consumo <= 20:
+        print("Plano Prata Fibra - 100Mbps")
         
     if consumo > 20:
-        print("Plano premium Fibra")
-
-    return consumo    
+        print("Plano Premium Fibra - 300Mbps")
+    
+    return consumo
+      
         
 
 # Solicita ao usuário que insira o consumo médio mensal de dados:
-consumo = float(input("Qual seu consumo: "))
+consumo = int(input("Qual seu consumo: "))
 # Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
-print(recomendar_plano())
+recomendar_plano(consumo)
